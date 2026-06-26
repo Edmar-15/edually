8d494c75ca24798dd4e76ca4f0574164eec19c12
Edmar-15 <0323-3644@lspu.edu.ph>
2026-06-26 19:25:31 +0800

    added tab components for slm page

--- slm/views.py
+++ slm/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.template.loader import render_to_string
 
 # Create your views here.
 @login_required(login_url='account:login')
 def slmlists(request):
-    return render(request, 'slm/slms.html')
+    tabs = [
+        {
+            "title": "Self Learning Modules",
+            "description":'All public modules you can enrol in',
+            "content": render_to_string("components/tabs/tab_self.html"),
+        },
+        {
+            "title": "My SLMs",
+            "description":'Your own created modules',
+            "content": render_to_string("components/tabs/tab_my_slms.html"),
+        },
+        {
+            "title": "My Learning Materials",
+            "description":'Notes, PDFs, videos you uploaded',
+            "content": render_to_string("components/tabs/tab_learning_materials.html"),
+        },
+        {
+            "title": "Public Learning Materials",
+            "description":'Resources shared by the community',
+            "content": render_to_string("components/tabs/tab_public_materials.html"),
+        },
+    ]
+    
+    return render(request, 'slm/slms.html', {"tabs": tabs})
