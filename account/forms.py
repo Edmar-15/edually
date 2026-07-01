277207de15b577deb407f0a3ec299489b285ae20
Kevin-Webdeve <0323-4526@lspu.edu.ph>
2026-07-02 07:47:16 +0800

    input type ,module content  section

--- account/forms.py
+++ account/forms.py
@@ -18,7 +18,7 @@ User = get_user_model()
 class LoginForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs.update({
+        self.fields['username'].widget = forms.EmailInput(attrs={
             'placeholder': 'Email Address',
             'autocomplete': 'username',
         })
--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,12 @@
+from django import forms
 from django.test import TestCase
 
-# Create your tests here.
+from .forms import LoginForm
+
+
+class LoginFormTests(TestCase):
+    def test_login_username_field_uses_email_input(self):
+        form = LoginForm()
+
+        self.assertIsInstance(form.fields["username"].widget, forms.EmailInput)
+        self.assertEqual(form.fields["username"].widget.attrs["autocomplete"], "username")
